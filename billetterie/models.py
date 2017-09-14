98b83d7b89faae1501e949de5f022460ef70d1cf
Philippe Vienne <philippegeek@gmail.com>
2017-09-14 22:53:02 +0200

    Start adding billetterie model

--- billetterie/models.py
+++ billetterie/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from ticketing.models import Event
+
+
+class Billetterie(models.Model):
+
+    event = models.ForeignKey(Event, verbose_name=_('Evènement'), related_name='entries')
+    title = models.CharField(max_length=255, verbose_name=_('Nom'))
+    miniature_background = models.ImageField(verbose_name=_("Miniature liste"), blank=True)
+    event_header = models.ImageField(verbose_name=_("En-tête billetterie"), blank=True)
+    displayed_date = models.DateTimeField()
+    description = models.TextField()
+    published = models.BooleanField()
+    access_mode = models.CharField(choices=('Public', 'Invitation', 'Gala', 'Code'))
+    entry_sold = models.ManyToManyField(Entry)
